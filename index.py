678dde192bfddb0c01787c4afd1d68bc1313c8cb
akosh567 <akosh3666@gmail.com>
2021-04-03 16:59:15 +0500

    U

--- index.py
+++ index.py
@@ -3,15 +3,16 @@
 # age_period = age + period
 # print ('Cherez',period,'let vam budet',age_period,'goda.')
 
-age = int(input('Skolko vam let: '))
+# age = int(input('Skolko vam let: '))
+# if age < 18:
+#     print ('Dostup zapreshen!')
+# elif age == 18:
+#     print ('Vam rovno 18 let!')
+# else:
+#     print ('Dostup razreshen!')   
+#     if age % 5 == 0:
+#         print ('Pozdravlaym u vas v etom godu yubiley!!!')
 
-if age < 18:
-    print ('Dostup zapreshen!')
-elif age == 18:
-    print ('Vam rovno 18 let!')
-else:
-    print ('Dostup razreshen!')   
-    if age % 5 == 0:
-        print ('Pozdravlaym u vas v etom godu yubiley!!!')
+
 
  
