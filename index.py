9bafa10783844e07ed000529a58b7570af0daeaf
akosh567 <akosh3666@gmail.com>
2021-04-03 16:58:24 +0500

    uzgarish

--- index.py
+++ index.py
@@ -1,2 +1,17 @@
-print ('hello world')
-print ('hello world')
+# age = int(input('Skolko vam let: '))
+# period = 15
+# age_period = age + period
+# print ('Cherez',period,'let vam budet',age_period,'goda.')
+
+age = int(input('Skolko vam let: '))
+
+if age < 18:
+    print ('Dostup zapreshen!')
+elif age == 18:
+    print ('Vam rovno 18 let!')
+else:
+    print ('Dostup razreshen!')   
+    if age % 5 == 0:
+        print ('Pozdravlaym u vas v etom godu yubiley!!!')
+
+ 
