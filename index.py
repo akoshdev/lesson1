c55b6758e750b225955b33b155ec86fc6bbc7063
akosh567 <akosh3666@gmail.com>
2021-04-06 17:38:22 +0500

    w

--- index.py
+++ index.py
@@ -1,88 +1,131 @@
-# age = int(input('Skolko vam let: '))
-# period = 15
-# age_period = age + period
-# print ('Cherez',period,'let vam budet',age_period,'goda.')
-
-# age = int(input('Skolko vam let: '))
-# if age < 18:
-#     print ('Dostup zapreshen!')
-# elif age == 18:
-#     print ('Vam rovno 18 let!')
-# else:
-#     print ('Dostup razreshen!')   
-#     if age % 5 == 0:
-#         print ('Pozdravlaym u vas v etom godu yubiley!!!')
-
-# number = 17
-# value = int(input('Vvedite chisel ot 1 do 100  ' ))
-# if value == number:
-#     print ('Vi ugadali')
-# else: 
-#     if value > number:
-#         print ('Nujno menshe')
-#     else:
-#         print ('Nujno bolshe')    
-
-# Kridit hisoblash uchun:
-
-
-# age = int(input('Kridit muddatini kiriting: '))
-# summa = int(input('Kridit summasini kiriting: '))
-# foiz = int(input('Kridit foizini kiriting: '))
-# qiymat = summa / age * foiz
-# qiyymat = qiymat / 12
-# print ('Oylik tulov summasi:',qiyymat)
-
-# name = 'Gvido'
-# name = input('Kto sozdatel Python: ')
+# # age = int(input('Skolko vam let: '))
+# # period = 15
+# # age_period = age + period
+# # print ('Cherez',period,'let vam budet',age_period,'goda.')
 #
-# while name != 'Gvido':
-#     print ('Ne verno')
-#     name = input('Kto sozdatel Python: ')
+# # age = int(input('Skolko vam let: '))
+# # if age < 18:
+# #     print ('Dostup zapreshen!')
+# # elif age == 18:
+# #     print ('Vam rovno 18 let!')
+# # else:
+# #     print ('Dostup razreshen!')
+# #     if age % 5 == 0:
+# #         print ('Pozdravlaym u vas v etom godu yubiley!!!')
 #
-# print ('Verno')
+# # number = 17
+# # value = int(input('Vvedite chisel ot 1 do 100  ' ))
+# # if value == number:
+# #     print ('Vi ugadali')
+# # else:
+# #     if value > number:
+# #         print ('Nujno menshe')
+# #     else:
+# #         print ('Nujno bolshe')
+#
+# # Kridit hisoblash uchun:
+#
+#
+# # age = int(input('Kridit muddatini kiriting: '))
+# # summa = int(input('Kridit summasini kiriting: '))
+# # foiz = int(input('Kridit foizini kiriting: '))
+# # qiymat = summa / age * foiz
+# # qiyymat = qiymat / 12
+# # print ('Oylik tulov summasi:',qiyymat)
+#
+# # name = 'Gvido'
+# # name = input('Kto sozdatel Python: ')
+# #
+# # while name != 'Gvido':
+# #     print ('Ne verno')
+# #     name = input('Kto sozdatel Python: ')
+# #
+# # print ('Verno')
+#
+#
+# # name = None
+# #
+# # while name != 'Gvido':
+# #     name = input('Kto sozdatel Python: ')
+# #     print('Ne verno')
+# #
+# # print ('Verno')
+#
+#
+# # name = None
+# #
+# # while name != 'Gvido':
+# #     name = input('Kto sozdatel Python: ')
+# #     if name == 'Gvido':
+# #         break
+# #     print('Ne verno')
+# #
+# # print ('Verno')
+#
+# # n = int(input('Vvedite lyuboe chislo: '))
+# # sum = n + 2
+# # print (sum)
+#
+#
+# # while True:
+# #     number = int(input('Vvedite chislo: '))
+# #     if number > 0 and number <= 10:
+# #         print (number**2)
+# #         break
+# #     else:
+# #         print ('Vvedite chislo ne menshe 10 i bolshe 0')
+#
+#
+# # top5 = 'Pervie 5 mest na sorevnavaniyax: 1. petrov 2. ivanov 3. siydirov 4. orlov 5. sokolov'
+# #
+# # start = top5.find('1')
+# # end = top5.find('4')
+# #
+# # top3 = top5[start:end]
+# #
+# # result = 'Pozdravlaym {} s uspexom!'.format(top3.upper())
+# #
+# # print(result)
+#
+# # ism = input('Uquvchi nomini kiriting: ')
+# # ism1 = ism
+# # yuqlama = input('Uquvchi darsdami: ')
 
 
-# name = None
-#
-# while name != 'Gvido':
-#     name = input('Kto sozdatel Python: ')
-#     print('Ne verno')
-#
-# print ('Verno')
 
 
-# name = None
+# ismlar = []
+# yuqlama = []
 #
-# while name != 'Gvido':
-#     name = input('Kto sozdatel Python: ')
-#     if name == 'Gvido':
-#         break
-#     print('Ne verno')
+# i = 0
+# while i < 5:
+#     # if i == 5:
+#     #     break
+#     ism = input('Uquvchi ismini kiriting: ')
+#     ismlar.append(ism)
+#     yuq = input('Uquvchi darsdami: ')
+#     yuqlama.append(yuq)
+#     i +=1
+# for j in range(len(ismlar)):
+#     print('{} darsda {}'. format(ismlar[j],yuqlama[j]))
 #
-# print ('Verno')
 
-# n = int(input('Vvedite lyuboe chislo: '))
-# sum = n + 2
-# print (sum)
-
-
-# while True:
-#     number = int(input('Vvedite chislo: '))
-#     if number > 0 and number <= 10:
-#         print (number**2)
-#         break
-#     else:
-#         print ('Vvedite chislo ne menshe 10 i bolshe 0')
+# for i in range(0,10):
+#     print(i)
+#
 
 
-# top5 = 'Pervie 5 mest na sorevnavaniyax: 1. petrov 2. ivanov 3. siydirov 4. orlov 5. sokolov'
-#
-# start = top5.find('1')
-# end = top5.find('4')
-#
-# top3 = top5[start:end]
-#
-# result = 'Pozdravlaym {} s uspexom!'.format(top3.upper())
-#
-# print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
